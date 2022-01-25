575438d0c879d86c05f5c00fbf1682ed05182352
Ian <68618201+ian-richard@users.noreply.github.com>
2022-01-25 20:04:50 +0000

    initial commit

---
+++ server/models/models.go
@@ -0,0 +1,5 @@
+package modelsimport "go.mongodb.org/mongo-driver/bson/primitive"type ToDoList struct {
+ 
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`  Task   string             `json:"task,omitempty"`
+	Status bool               `json:"status,omitempty"`
+  }
